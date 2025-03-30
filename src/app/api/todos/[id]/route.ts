49c70cda78349d9fdcf106c058c63c9e3ea534dc
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-03-29 20:58:20 -0500

    feat: implement GET and PUT API routes for managing todos with validation

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,8 @@
 import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
+import { Segment } from 'next/dist/server/app-render/types';
 import { NextResponse } from 'next/server';
+import { boolean, object, string } from 'yup';
 
 interface argsTodo {
 	params: {
@@ -7,17 +10,55 @@ interface argsTodo {
 	};
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+	const todo = await prisma.todo.findUnique({ where: { id } });
+
+	return todo;
+};
+
 export async function GET(
 	request: Request,
 	{ params }: argsTodo
 ): Promise<Response> {
-	const todoId = params.id;
-	const todo = await prisma.todo.findUnique({ where: { id: todoId } });
+	const { id } = params;
+	const todo = await getTodo(id);
 
 	if (!todo)
-		return NextResponse.json({
-			error: `El todo con el id ${todoId} no existe`,
-		});
+		return NextResponse.json(
+			{ error: `El todo con el id ${id} no existe` },
+			{ status: 404 }
+		);
 
 	return NextResponse.json({ todo });
+}
+
+const putSchema = object({
+	completed: boolean().optional(),
+	description: string().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segment) {
+	const { id } = params;
+	const todo = await getTodo(id);
+
+	if (!todo)
+		return NextResponse.json(
+			{ error: `El todo con el id ${id} no existe` },
+			{ status: 404 }
+		);
+
+	try {
+		const { completed, description } = await putSchema.validate(
+			await request.json()
+		);
+
+		const updatedTodo = prisma.todo.update({
+			where: { id },
+			data: { completed, description },
+		});
+
+		return NextResponse.json(updatedTodo);
+	} catch (error) {
+		return NextResponse.json(error, { status: 400 });
+	}
 }
