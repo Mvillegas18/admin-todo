3dc34936302ddd7342455605f3d1887441db06ea
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-03-30 20:34:36 -0500

    feat: configure remote image patterns for optimized loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'tailus.io',
+				port: '',
+				pathname:
+					'/sources/blocks/stats-cards/preview/images/second_user.webp',
+				search: '',
+			},
+		],
+	},
 };
 
 export default nextConfig;
