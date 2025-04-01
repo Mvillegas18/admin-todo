af62b5ac716ca53eafdfa26f05ace344f4d67d52
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-01 18:18:43 -0500

    feat: integrate toggleTodo function in TodosGrid for updating todo completion status

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import * as todosApi from '@/todos/helpers/todos';
 import { Todo } from '@prisma/client';
+import { useRouter } from 'next/navigation';
 import { TodoItem } from './TodoItem';
 
 interface Props {
@@ -8,10 +10,18 @@ interface Props {
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
+	const router = useRouter();
+
+	const toggleTodo = async (id: string, completed: boolean) => {
+		const updatedTodo = await todosApi.updateTodo(id, completed);
+		console.log({ updatedTodo });
+
+		router.refresh();
+	};
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
 			{todos.map((todo) => (
-				<TodoItem key={todo.id} todo={todo} />
+				<TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
 			))}
 		</div>
 	);
