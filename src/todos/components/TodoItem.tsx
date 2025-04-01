b24b38d01381bcc6a6e3e2dab90e42169668db60
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-01 18:18:39 -0500

    feat: add toggleTodo function to TodoItem for updating completion status

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 import { Todo } from '@prisma/client';
 import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 
 interface Props {
 	todo: Todo;
+	toggleTodo: (id: string, completed: boolean) => Promise<Todo | void>;
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
 	const todoPending = todo.completed
 		? 'line-through bg-blue-50 rounded-lg shadow-sm p-5 border-dashed border border-blue-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0'
 		: 'bg-red-50 rounded-lg shadow-sm p-5 border-dashed border border-red-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0';
@@ -14,6 +17,7 @@ export const TodoItem = ({ todo }: Props) => {
 		<div className={todoPending}>
 			<div className="flex flex-col sm:flex-row justify-start items-center gap-4 ">
 				<div
+					onClick={() => toggleTodo(todo.id, !todo.completed)}
 					className={`flex p-2 rounded-md cursor-pointer ${
 						todo.completed
 							? 'hover:bg-blue-200'
