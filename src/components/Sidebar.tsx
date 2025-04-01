a25f363dc03682f181fba2361b2f441e08620141
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-03-31 20:53:40 -0500

    feat: update Sidebar component with new logos and user details

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,28 +7,28 @@ export function Sidebar() {
 	return (
 		<aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
 			<div>
-				<div className="-mx-6 px-6 py-4">
-					<Link href="#" title="home">
+				<div className="mx-6  py">
+					<Link href="/dashboard" title="home">
 						<Image
-							src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
-							className="w-32"
+							src="/logo-miguel.webp"
+							className="w-80"
 							alt="tailus logo"
-							width={150}
-							height={150}
+							width={2048}
+							height={2048}
 						/>
 					</Link>
 				</div>
 
 				<div className="mt-8 text-center">
 					<Image
-						src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+						src="/foto-admin.webp"
 						alt=""
 						className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
 						width={150}
 						height={150}
 					/>
 					<h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-						Cynthia J. Watts
+						Miguel A. Villegas
 					</h5>
 					<span className="hidden text-gray-400 lg:block">Admin</span>
 				</div>
