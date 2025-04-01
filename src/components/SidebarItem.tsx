cf62d5322bb7d8a7609350da39f619f0205e88c9
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-01 16:05:25 -0500

    feat: refactor SidebarItem component to accept dynamic icon and title props

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,18 +1,28 @@
-import Link from 'next/link';
-import { CiBookmarkCheck } from 'react-icons/ci';
+'use client';
 
-export function SidebarItem() {
-	{
-		/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */
-	}
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export interface SidebarProps {
+	icon: React.ReactNode;
+	path: string;
+	title: string;
+}
+
+export function SidebarItem({ icon, path, title }: SidebarProps) {
+	const pathName = usePathname();
+
 	return (
 		<li>
 			<Link
-				href="/dashboard/rest-todos"
-				className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+				href={path}
+				className={`px-4 py-3 flex items-center space-x-4 rounded-md  group hover:bg-gradient-to-r hover:bg-sky-600 hover:text-white ${
+					pathName === path &&
+					'text-white bg-gradient-to-r from-sky-600 to-cyan-400 transition-all '
+				}`}
 			>
-				<CiBookmarkCheck size={30} />
-				<span className="group-hover:text-gray-700">Categories</span>
+				{icon}
+				<span className="group-hover:text-white-700">{title}</span>
 			</Link>
 		</li>
 	);
