6ad07bd4396b0794563bdb682c12b1b4a667b59f
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-04 15:59:00 -0500

    feat: implement addTodo and deleteCompleted functions for todo management

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -22,3 +22,36 @@ export const toggleTodo = async (
 	revalidatePath('/dashboard/server-todos');
 	return updatedTodo;
 };
+
+export const addTodo = async (
+	description: string
+): Promise<Todo | { message: string }> => {
+	try {
+		const todo = await prisma.todo.create({
+			data: { description },
+		});
+
+		revalidatePath('/dashboard/server-todos');
+
+		return todo;
+	} catch (error) {
+		return {
+			message: 'Error al crear un todo',
+		};
+	}
+};
+
+export const deleteCompleted = async () => {
+	try {
+		const todo = await prisma.todo.deleteMany({
+			where: { completed: true },
+		});
+
+		revalidatePath('/dashboard/server-todos');
+		return todo;
+	} catch (error) {
+		return {
+			message: 'Error al eliminar los todos completados',
+		};
+	}
+};
