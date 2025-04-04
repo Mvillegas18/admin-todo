441eaba0784356a029e25889ee6352243563d8f1
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-03 19:48:41 -0500

    feat: refactor TodosGrid to import toggleTodo action and comment out local implementation

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -4,6 +4,7 @@ import * as todosApi from '@/todos/helpers/todos';
 import { Todo } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import { TodoItem } from './TodoItem';
+import { toggleTodo } from '../actions/todo-actions';
 
 interface Props {
 	todos?: Todo[];
@@ -12,11 +13,11 @@ interface Props {
 export const TodosGrid = ({ todos = [] }: Props) => {
 	const router = useRouter();
 
-	const toggleTodo = async (id: string, completed: boolean) => {
-		const updatedTodo = await todosApi.updateTodo(id, completed);
+	// const toggleTodo = async (id: string, completed: boolean) => {
+	// 	const updatedTodo = await todosApi.updateTodo(id, completed);
 
-		router.refresh();
-	};
+	// 	router.refresh();
+	// };
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
 			{todos.map((todo) => (
