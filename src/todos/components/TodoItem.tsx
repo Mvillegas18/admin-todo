484c3c036c9a71fb215447371629008f193a18bc
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-04 16:38:34 -0500

    feat: implement optimistic UI for todo completion in TodoItem component

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Todo } from '@prisma/client';
+import { startTransition, useOptimistic } from 'react';
 import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 
 interface Props {
@@ -9,7 +10,29 @@ interface Props {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
-	const todoPending = todo.completed
+	const [todoOptimistic, toggleTodoOptimistic] = useOptimistic(
+		todo,
+		(state, newCompleteValue: boolean) => ({
+			...state,
+			completed: newCompleteValue,
+		})
+	);
+
+	const onToggleTodo = async () => {
+		try {
+			startTransition(() =>
+				toggleTodoOptimistic(!todoOptimistic.completed)
+			);
+
+			await toggleTodo(todoOptimistic.id, !todoOptimistic.completed);
+		} catch (error) {
+			startTransition(() =>
+				toggleTodoOptimistic(!todoOptimistic.completed)
+			);
+		}
+	};
+
+	const todoPending = todoOptimistic.completed
 		? 'line-through bg-blue-50 rounded-lg shadow-sm p-5 border-dashed border border-blue-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0'
 		: 'bg-red-50 rounded-lg shadow-sm p-5 border-dashed border border-red-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0';
 
@@ -17,14 +40,17 @@ export const TodoItem = ({ todo, toggleTodo }: Props) => {
 		<div className={todoPending}>
 			<div className="flex flex-col sm:flex-row justify-start items-center gap-4 ">
 				<div
-					onClick={() => toggleTodo(todo.id, !todo.completed)}
+					// onClick={() =>
+					// 	toggleTodo(todoOptimistic.id, !todoOptimistic.completed)
+					// }
+					onClick={onToggleTodo}
 					className={`flex p-2 rounded-md cursor-pointer ${
-						todo.completed
+						todoOptimistic.completed
 							? 'hover:bg-blue-200'
 							: 'hover:bg-red-200'
 					} hover:bg-blue-100`}
 				>
-					{todo.completed ? (
+					{todoOptimistic.completed ? (
 						<IoCheckboxOutline size={30} />
 					) : (
 						<IoSquareOutline size={30} />
@@ -32,7 +58,7 @@ export const TodoItem = ({ todo, toggleTodo }: Props) => {
 				</div>
 
 				<div className="text-center sm:text-left">
-					{todo.description}
+					{todoOptimistic.description}
 				</div>
 			</div>
 		</div>
