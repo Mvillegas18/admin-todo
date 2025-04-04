992997c8aa6a73529ff8fe6259f91c5203e31ad3
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-03 19:48:11 -0500

    feat: create ServerActions component to manage and display todos

---
+++ src/app/dashboard/server-actions/page.tsx
@@ -0,0 +1,26 @@
+import prisma from '@/lib/prisma';
+import { NewTodo } from '@/todos/components/NewTodo';
+import { TodosGrid } from '@/todos/components/TodosGrid';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Server actions',
+	description: 'Descripcion de server actions',
+};
+
+export default async function ServerActions() {
+	const todos = await prisma.todo.findMany({
+		orderBy: { description: 'asc' },
+	});
+
+	return (
+		<>
+			<span className="text-3xl mb-10">Server Actions</span>
+			<div className="w-full px-3 mx-5 mb-5">
+				<NewTodo />
+			</div>
+
+			<TodosGrid todos={todos} />
+		</>
+	);
+}
