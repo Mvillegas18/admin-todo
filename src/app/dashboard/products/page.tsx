9f6a8fd70e147acc5009aa476fe1153bfddfaf28
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-06 17:40:35 -0500

    feat: update ProductsPage to display a list of products using ProductCard component

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,4 +1,5 @@
 import { ProductCard } from '@/products/components/ProductCard';
+import { products } from '@/products/data/products';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function ProductsPage() {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-			<ProductCard />
+			{products.map((product) => (
+				<ProductCard key={product.id} {...product} />
+			))}
 		</div>
 	);
 }
