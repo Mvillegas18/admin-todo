ab55c3b82e858ed7b5cf9b4167fae4685d2a4a01
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-05 21:19:44 -0500

    feat: create ProductsPage and ProductCard components for product display

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,15 @@
+import { ProductCard } from '@/products/components/ProductCard';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Products',
+	description: 'Products page',
+};
+
+export default function ProductsPage() {
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+			<ProductCard />
+		</div>
+	);
+}
