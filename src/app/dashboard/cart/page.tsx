ac401e6952e6b209c91756b4d12f23f7b34f44d3
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-06 19:08:00 -0500

    feat: add CartPage component to display products in the shopping cart

---
+++ src/app/dashboard/cart/page.tsx
@@ -0,0 +1,67 @@
+import { products, type Product } from '@/products/data/products';
+import { ItemCart } from '@/shoping-cart/components/ItemCart';
+import { Metadata } from 'next';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+	title: 'Carrito de compras',
+	description: 'Productos en el carrito',
+};
+
+interface ProductInCart {
+	product: Product;
+	cuantity: number;
+}
+
+const getProductsInCart = (cart: { [id: string]: number }): ProductInCart[] => {
+	const ProductInCart: ProductInCart[] = [];
+
+	for (const id of Object.keys(cart)) {
+		const product = products.find((product) => product.id === id);
+		if (product) {
+			ProductInCart.push({
+				product,
+				cuantity: cart[id],
+			});
+		}
+	}
+
+	return ProductInCart;
+};
+
+export default async function CartPage() {
+	const cookieStore = cookies();
+	const cart = JSON.parse((await cookieStore).get('cart')?.value ?? '{}') as {
+		[id: string]: number;
+	};
+
+	const productsInCart = getProductsInCart(cart);
+
+	return (
+		<div className="flex flex-col gap-3">
+			<div className="flex items-center justify-between mb-2">
+				<h1 className="text-5xl ">Productos en el carrito</h1>
+				<Link
+					href={'/dashboard/products'}
+					className=" hover:underline bg-blue-500  text-white font-bold py-2 px-4 rounded"
+				>
+					Ver productos
+				</Link>
+			</div>
+			<hr className="mb-2" />
+
+			<div className="flex flex-col sm:flex-row gap-2 w-full">
+				<div className="flex flex-col gap-2 w-full sm:w-8/12">
+					{productsInCart.map(({ product, cuantity }) => (
+						<ItemCart
+							key={product.id}
+							product={product}
+							quantity={cuantity}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
