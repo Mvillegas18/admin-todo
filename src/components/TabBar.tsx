ad7e09bf3625903b89f488da09f0ac9deee9162d
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-04-07 21:43:01 -0500

    feat: add router refresh on tab change in TabBar component

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { setCookie } from 'cookies-next/client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 interface Props {
@@ -12,11 +13,13 @@ export const TabBar = ({
 	tabOptions = [1, 2, 3, 4],
 	currentTab = 1,
 }: Props) => {
+	const router = useRouter();
 	const [setselected, setSetselected] = useState(currentTab);
 
 	const handleTabChange = (tab: number) => {
 		setSetselected(tab);
 		setCookie('tab', tab.toString());
+		router.refresh();
 	};
 
 	return (
